ebec3ce747b49edb78a064824ac51eb4cf02f344
Mahmoud ElKhlafawi <mahmoudmagdi@ymail.com>
2024-05-31 23:37:34 +0300

    Adapt error and loading overlays

--- components/UI/ErrorOverlay.tsx
+++ components/UI/ErrorOverlay.tsx
@@ -1,19 +1,40 @@
-import {View, Text} from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
 
-import React from 'react';
-import {errorStyles} from './ErrorStyle.tsx';
-interface Props {
+import React from "react";
+import { GlobalStyle } from "../../constants/styles.tsx";
+
+interface ErrorOverlayProps {
   message: string;
 }
-function ErrorOverlay({message}: Props): React.JSX.Element {
+
+function ErrorOverlay({ message }: ErrorOverlayProps): React.JSX.Element {
   return (
-    <View style={errorStyles.container}>
-      <Text style={[errorStyles.text, errorStyles.title]}>
+    <View style={styles.container}>
+      <Text style={[styles.text, styles.title]}>
         An error occurred!
       </Text>
-      <Text style={errorStyles.text}>{message}</Text>
+      <Text style={styles.text}>{message}</Text>
     </View>
   );
 }
 
 export default ErrorOverlay;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 24,
+    height: 650
+  },
+  text: {
+    color: GlobalStyle.colors.black,
+    textAlign: "center",
+    marginBottom: 8,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: GlobalStyle.colors.black
+  }
+});
