3af644e2dc38d51ecb9a8d0e8611c09c9e20c25f
Mahmoud ElKhlafawi <mahmoudmagdi@ymail.com>
2024-06-02 23:12:00 +0300

    Update the icon and action as per Realm database

--- screens/MovieDetailsScreen.tsx
+++ screens/MovieDetailsScreen.tsx
@@ -1,13 +1,20 @@
 import {View, Text, StyleSheet, Image, ScrollView} from 'react-native';
-import React, {useContext, useLayoutEffect} from 'react';
+import React, {useContext, useEffect, useLayoutEffect, useState} from 'react';
 import {MoviesContext} from '../store/context/movies-context';
 import {SelectedFilterContext} from '../store/context/selected-filter-context';
-import {useDispatch, useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import Movie from '../model/movie';
 
 import MovieDetails from '../components/moviesOutput/MovieDetails';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {addFavorite, removeFavorite} from '../store/redux/favorites.tsx';
+import {
+  addMovieToFavorites,
+  getFavoriteMovies,
+  isMovieFavorite,
+  removeMovieFromFavorites,
+} from '../store/realm/database.tsx';
+import realm from '../store/realm/realmConfig.tsx';
 
 type MovieOverViewScreenProps = {
   route: any;
@@ -46,11 +53,11 @@ function MovieDetailsScreen({
       movieItem = null;
   }
 
-  const favoriteMovies = useSelector(
-    (state: any) => state.favoriteMovies.favoriteMovies,
-  );
-  const isFavorite = favoriteMovies.some(
-    (movie: Movie) => movie.id === movieItem?.id,
+  // const favoriteMovies = useSelector(
+  //   (state: any) => state.favoriteMovies.favoriteMovies
+  // );
+  const [isFavorite, setIsFavorite] = useState(
+    movieItem?.id ? isMovieFavorite(movieItem?.id) : false,
   );
   const dispatch = useDispatch();
 
@@ -59,9 +66,21 @@ function MovieDetailsScreen({
     if (isFavorite) {
       // remove from favorites
       dispatch(removeFavorite(movieItem));
+
+      // remove from realm
+      movieItem?.id && removeMovieFromFavorites(movieItem?.id);
+
+      // update favorite status
+      setIsFavorite(false);
     } else {
       // add to favorites
       dispatch(addFavorite(movieItem));
+
+      // add to realm
+      addMovieToFavorites(movieItem);
+
+      // update favorite status
+      setIsFavorite(true);
     }
   }
 
