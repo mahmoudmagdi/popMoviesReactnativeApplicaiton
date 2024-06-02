534992b9110f64653a25b9b2033975c4fdb36604
Mahmoud ElKhlafawi <mahmoudmagdi@ymail.com>
2024-06-02 22:12:32 +0300

    Fetch favorites to the favorites screen

--- screens/bottomTabs/FavoritesScreen.tsx
+++ screens/bottomTabs/FavoritesScreen.tsx
@@ -1,18 +1,11 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import React from 'react';
+import MoviesList from '../../components/moviesOutput/MoviesList.tsx';
+import {useSelector} from 'react-redux';
 
 export default function FavoritesScreen(): React.JSX.Element {
-  return (
-    <View style={styles.container}>
-      <Text>Favorites Screen</Text>
-    </View>
+  const favoriteMovies = useSelector(
+    (state: any) => state.favoriteMovies.favoriteMovies,
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  }
-});
+  return <MoviesList movies={favoriteMovies} isFavoritesScreen={true} />;
+}
