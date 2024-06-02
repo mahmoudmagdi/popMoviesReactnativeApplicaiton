15b80b19af98d087b1c514560c3bf6c241f8729d
Mahmoud ElKhlafawi <mahmoudmagdi@ymail.com>
2024-06-02 23:11:45 +0300

    Create required schemas for realm DB

--- model/movie.tsx
+++ model/movie.tsx
@@ -1,52 +1,42 @@
-import Genres from "./genres.tsx";
+import Realm, {ObjectSchema} from 'realm';
+import Genres from './genres';
 
-class Movie {
-  id: number;
-  title: string;
-  adult: boolean;
-  backdrop_path: string;
-  genres: Genres[];
-  original_language: string;
-  overview: string;
-  popularity: number;
-  poster_path: string;
-  release_date: string;
-  revenue: number;
-  tagline: string;
-  vote_average: number;
-  vote_count: number;
+class Movie extends Realm.Object<Movie> {
+  id!: number;
+  title?: string;
+  adult?: boolean;
+  backdrop_path?: string;
+  genres?: Genres[];
+  original_language?: string;
+  overview?: string;
+  popularity?: number;
+  poster_path?: string;
+  release_date?: string;
+  revenue?: number;
+  tagline?: string;
+  vote_average?: number;
+  vote_count?: number;
 
-  constructor(
-    id: number,
-    title: string,
-    adult: boolean,
-    backdrop_path: string,
-    genres: Genres[],
-    original_language: string,
-    overview: string,
-    popularity: number,
-    poster_path: string,
-    release_date: string,
-    revenue: number,
-    tagline: string,
-    vote_average: number,
-    vote_count: number
-  ) {
-    this.id = id;
-    this.title = title;
-    this.adult = adult;
-    this.backdrop_path = backdrop_path;
-    this.genres = genres;
-    this.original_language = original_language;
-    this.overview = overview;
-    this.popularity = popularity;
-    this.poster_path = poster_path;
-    this.release_date = release_date;
-    this.revenue = revenue;
-    this.tagline = tagline;
-    this.vote_average = vote_average;
-    this.vote_count = vote_count;
-  }
+  static schema: ObjectSchema = {
+    name: 'Movie',
+    properties: {
+      id: 'int',
+      title: 'string?',
+      adult: 'bool?',
+      backdrop_path: 'string?',
+      genres: 'Genres[]',
+      original_language: 'string?',
+      overview: 'string?',
+      popularity: 'double?',
+      poster_path: 'string?',
+      release_date: 'string?',
+      revenue: 'int?',
+      tagline: 'string?',
+      vote_average: 'double?',
+      vote_count: 'int?',
+    },
+    primaryKey: 'id',
+  };
 }
 
 export default Movie;
