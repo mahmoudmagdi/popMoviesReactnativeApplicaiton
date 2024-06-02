77dc72948e734fa95fb9374ddf42c77a12d6519d
Mahmoud ElKhlafawi <mahmoudmagdi@ymail.com>
2024-06-02 23:12:11 +0300

    Fetch movies from realm DB

--- screens/bottomTabs/FavoritesScreen.tsx
+++ screens/bottomTabs/FavoritesScreen.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import MoviesList from '../../components/moviesOutput/MoviesList.tsx';
-import {useSelector} from 'react-redux';
+import React, {useEffect, useState} from 'react';
+import MoviesList from '../../components/moviesOutput/MoviesList';
+import {getFavoriteMovies} from '../../store/realm/database';
+import realm from '../../store/realm/realmConfig';
+import Movie from '../../model/movie.tsx';
 
 export default function FavoritesScreen(): React.JSX.Element {
-  const favoriteMovies = useSelector(
-    (state: any) => state.favoriteMovies.favoriteMovies,
-  );
+  const [favoriteMovies, setFavoriteMovies] = useState<
+    Realm.Results<Movie> | []
+  >([]);
 
-  return <MoviesList movies={favoriteMovies} isFavoritesScreen={true} />;
+  useEffect(() => {
+    const fetchFavoriteMovies = () => {
+      const movies = getFavoriteMovies();
+      setFavoriteMovies(movies);
+    };
+
+    fetchFavoriteMovies();
+
+    // Optionally, you can set up a listener for database changes
+    const realmListener = () => {
+      fetchFavoriteMovies();
+    };
+
+    realm.addListener('change', realmListener);
+
+    // Clean up the listener on unmount
+    return () => {
+      realm.removeListener('change', realmListener);
+    };
+  }, []);
+
+  return (
+    <MoviesList movies={favoriteMovies as Movie[]} isFavoritesScreen={true} />
+  );
 }
