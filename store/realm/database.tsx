db9d55845bbdfa42a339323e3d577daa1658ff24
Mahmoud ElKhlafawi <mahmoudmagdi@ymail.com>
2024-06-04 16:13:36 +0300

    Remove unwanted console logs

--- store/realm/database.tsx
+++ store/realm/database.tsx
@@ -1,15 +1,14 @@
-import realm from './realmConfig';
-import Movie from '../../model/movie.tsx';
+import realm from "./realmConfig";
+import Movie from "../../model/movie.tsx";
 
 // Add movie to favorites
 export const addMovieToFavorites = (movie: Movie | null): void => {
   try {
     realm.write(() => {
-      realm.create('Movie', {...movie});
+      realm.create("Movie", { ...movie });
     });
-    console.log('Movie added to favorites!');
   } catch (error) {
-    console.log('Error on adding movie to favorites: ', error);
+    console.log("Error on adding movie to favorites: ", error);
   }
 };
 
@@ -17,34 +16,33 @@ export const addMovieToFavorites = (movie: Movie | null): void => {
 export const removeMovieFromFavorites = (movieId: number): void => {
   try {
     realm.write(() => {
-      let movie = realm.objectForPrimaryKey<Movie>('Movie', movieId);
+      let movie = realm.objectForPrimaryKey<Movie>("Movie", movieId);
       if (movie) {
         realm.delete(movie);
-        console.log('Movie removed from favorites!');
       } else {
-        console.log('Movie not found!');
+        console.log("Movie not found!");
       }
     });
   } catch (error) {
-    console.log('Error on removing movie from favorites: ', error);
+    console.log("Error on removing movie from favorites: ", error);
   }
 };
 
 // Fetch favorite movies
 export const getFavoriteMovies = (): Realm.Results<Movie> => {
   try {
-    return realm.objects<Movie>('Movie');
+    return realm.objects<Movie>("Movie");
   } catch (error) {
-    console.log('Error on fetching favorite movies: ', error);
+    console.log("Error on fetching favorite movies: ", error);
     return [] as any;
   }
 };
 
 export const isMovieFavorite = (movieId: number): boolean => {
   try {
-    return !!realm.objectForPrimaryKey<Movie>('Movie', movieId);
+    return !!realm.objectForPrimaryKey<Movie>("Movie", movieId);
   } catch (error) {
-    console.log('Error on checking if movie is favorite: ', error);
+    console.log("Error on checking if movie is favorite: ", error);
     return false;
   }
 };
